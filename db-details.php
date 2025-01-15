7e9ce5c9f3d8cc8b8b4aa2a523eab171e4be69c6
Nelson Musonda <nelsonmusonda001@gmail.com>
2025-01-15 10:55:19 +0200

    Made inprovements

--- db-details.php
+++ db-details.php
@@ -2,7 +2,9 @@
    //READ THOROUGH AND COMMENT/UNCOMMENT LINES ACCORDINGLY
    //CHECK THE HEADING DISCRIPTION FOR NON EMPTY SEQUENCED LINES
 
-   /*// DETAILS FOR INFINITY FREE
+
+
+    /*// DETAILS FOR INFINITY FREE, COMMENT IF NOT THE PLATFORM
    $dbHost = "sql308.infinityfree.com";
    $dbUser = "if0_37541813";
    $dbName = "if0_37541813_interlink";
@@ -10,23 +12,30 @@
    mysqli($dbHost, $dbUser, $dbPass, $dbName);
    */
    
-   // DETAILS FOR XAMPP OR TREMUX
+
+
+   // DETAILS FOR XAMPP OR TREMUX, COMMENT IF NOT THE PLATFORM
    $dbHost = "localhost";
    $dbUser = "root";
    $dbName = "interlink";
    $dbPass = "";
 
-   //ADDITIONAL DETAILS FOR TERMUX
-   $dbPort = 3306;
-   $dbSocket =  "/data/data/com.termux/files/usr/var/run/mysqld.sock";
-   
-   
-   /*// CONNECTION FOR XAMPP AND INFINITYFREE
-   $conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
-   */
 
-   // CONMECTION FOR TERMUX
-   $conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName, $dbPort, $dbSocket);
+
+   //ADDITIONAL DETAILS FOR TERMUX, ALSO REQUIRED, COMMENT IF NOT THE PLATFORM
+   //$dbPort = 3306;
+   //$dbSocket =  "/data/data/com.termux/files/usr/var/run/mysqld.sock";
+   
+   
+
+   // CONNECTION FOR XAMPP AND INFINITYFREE, COMMENT IF NOT THE PLATFORM
+   $conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+   
+
+
+
+   // CONMECTION FOR TERMUX, COMMENT IF NOT THE PLATFORM
+   //$conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName, $dbPort, $dbSocket);
 
 
    if($conn->connect_error){
